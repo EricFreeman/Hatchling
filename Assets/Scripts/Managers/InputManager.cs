e4a63e7934d896f5d6cb41de1e50de34023e5e03
Eric Freeman <eric.freeman@extendhealth.com>
2014-06-09 08:13:09 -0600

    Added player and background to scene.  Added player script for jumping.

---
+++ Assets/Scripts/Managers/InputManager.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+namespace Assets.Scripts
+{
+    public static class InputManager
+    {
+        public static bool IsKeyDownOrTouch(KeyCode key, int finger = 0)
+        {
+            return Input.GetKeyDown(key) ||
+                   (Input.touchCount == 1 && Input.GetTouch(finger).phase == TouchPhase.Began);
+        }
+    }
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+namespace Assets.Scripts
+{
+    public class Player : MonoBehaviour
+    {
+        public float JumpHeight = 500;
+
+        void Update ()
+        {
+            if (InputManager.IsKeyDownOrTouch(KeyCode.Space))
+            {
+                transform.rigidbody2D.velocity = Vector2.zero;
+                transform.rigidbody2D.AddForce(new Vector2(0, JumpHeight));
+            }
+        }
+    }
+}
