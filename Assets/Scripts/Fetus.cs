082b33d1855de3daba6b4653b26c256b695cd6dd
Eric Freeman <eric.freeman@extendhealth.com>
2014-06-10 17:27:40 -0600

    Committing last of stuff I was working on on the train ride home

---
+++ Assets/Scripts/Fetus.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+namespace Assets.Scripts
+{
+    public class Fetus : MonoBehaviour
+    {
+        public bool DestroyFlag = false;
+
+        void Update()
+        {
+            if (DestroyFlag) DestroyImmediate(gameObject);
+        }
+
+        private void OnTriggerEnter2D(Collider2D c)
+        {
+            DestroyFlag = true;
+        }
+    }
+}
