3fb0893f9b34f0291ad83f0cc9ba3e0c0ff0027f
Eric Freeman <eric.freeman@extendhealth.com>
2014-06-10 17:11:58 -0600

    Sway the parts of the dragon back and forth to make it harder to dodge bombs.

---
+++ Assets/Scripts/Movement/SwayMovement.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+namespace Assets.Scripts.Movement
+{
+    public class SwayMovement : MonoBehaviour
+    {
+        void Update()
+        {
+            transform.Translate(new Vector2(0, .01f * Mathf.Cos(Time.time)));
+        }
+    }
+}
