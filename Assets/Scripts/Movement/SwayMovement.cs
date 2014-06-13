b2368cd397d4304b23eb5cc155b6251537e3a09f
Eric Freeman <eric.freeman@extendhealth.com>
2014-06-12 23:42:21 -0600

    Improved the sway a bit to make it look better and make the game more challenging when weaving through bombs.

--- Assets/Scripts/Movement/SwayMovement.cs
+++ Assets/Scripts/Movement/SwayMovement.cs
@@ -4,10 +4,16 @@ namespace Assets.Scripts.Movement
 {
     public class SwayMovement : MonoBehaviour
     {
+        public float maxUpDown = .01f;
+        public float angle = 2.5f;
+        public float speed = 10;
+        public float toDegrees = 0.5235988f;    // 30 degrees in radians
+
         void Update()
         {
-            // TODO: Fix this to make it not suck
-//            transform.Translate(new Vector2(0, .01f * Mathf.Cos(Time.time)));
+            angle += speed * Time.deltaTime;
+            if (angle > 360) angle -= 360;
+            transform.position += new Vector3(0, maxUpDown * Mathf.Sin(angle * toDegrees), 0);
         }
     }
 }
